4bbfc93df48082d30dec632866f2f1850160d540
chenhuiyi <chenhuiyi@qinsilk.com>
2023-02-24 13:59:00 +0800

    2.24上午补充认识Node，任何一个模块(js文件)都被一个外层函数所包裹

--- 01_认识Node.js/01_hello.js
+++ 01_认识Node.js/01_hello.js
@@ -1,7 +1,18 @@
-console.log("hello NodeJs");
+// console.log("hello NodeJs");
 
-function test() {
-  console.log("哈哈哈");
-}
+// function test() {
+//   console.log("哈哈哈");
+// }
 
-test();
+// test();
+
+// 如何在一个函数体里，输出函数本身？
+
+
+// 2、Node中任何一个模块(js文件)都被一个外层函数所包裹
+console.log(arguments.callee.toString());
+
+// function (exports, require, module, __filename, __dirname) { 
+//   /* 你所写的文件里面的内容 */
+// }
+
